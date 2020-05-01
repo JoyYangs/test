0a631f7616034eb3a2985f51a49f02979269301b
YJoye <18292307391@163.com>
2020-05-01 18:12:02 +0800

    tabbar

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="tab-bar-item" @click="itemClick">
+		<div>
+			<slot name="normal"></slot>
+		</div>
+		<div>
+			<slot name="selected"></slot>
+		</div>
+		<div>
+			<slot name="text"></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "TabBarItem",
+		methods: {
+			itemClick() {
+				console.log("itemClick")
+			}
+		}
+	}
+</script>
+
+<style>
+	.tab-bar-item {
+		flex: 1;
+		height: 49px;
+	}
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<tab-bar>
+			<tab-bar-item v-for="item in items">
+				<div slot="normal"></div>
+				<div slot="selected"></div>
+				<div slot="text">{{item.text}}</div>
+			</tab-bar-item>
+		</tab-bar>
+	</div>
+</template>
+
+<script>
+	import { TabBar, TabBarItem } from '../../common/tabbar/index'
+
+	export default {
+		name: "MainTabBar",
+		components: {
+			TabBar,
+			TabBarItem
+		}, 
+		data() {
+			return {
+				items: [{
+						text: "首页",
+						image: "",
+						selImage: "" 
+					}, {
+						text: "分类",
+						image: "",
+						selImage: "" 
+					}, {
+						text: "购物车",
+						image: "",
+						selImage: "" 
+					},{
+						text: "我的",
+						image: "",
+						selImage: ""
+					}
+				]
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
